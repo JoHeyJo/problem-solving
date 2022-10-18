18bec1f855b642dc65a5d07c1172924ebf103cba
jf <jpf0628@gmail.com>
2022-10-18 13:05:10 -0700

    #30: 226. Invert Binary Tree

---
+++ LeetCode/__tests__/invertTree.test.js
@@ -0,0 +1,15 @@
+// "use strict";
+
+// const { invertTree } = require('../invertTree');
+
+// test("Given the root of a binary tree, invert the tree, and return its root..",
+//   function () {
+//     const result = invertTree([4, 2, 7, 1, 3, 6, 9]);
+//     expect(result).toEqual([4, 7, 2, 9, 6, 3, 1]);
+//   })
+
+// test("Given the root of a binary tree, invert the tree, and return its root..",
+//   function () {
+//     const result = invertTree([2, 1, 3]);
+//     expect(result).toEqual([2, 3, 1]);
+//   })
