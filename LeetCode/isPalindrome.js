d30fa6607edc082f7d56a931ab5c06d1c06e4a78
jf <jpf0628@gmail.com>
2022-12-01 15:05:57 -0800

    review

--- LeetCode/isPalindrome.js
+++ LeetCode/isPalindrome.js
@@ -16,4 +16,30 @@ var isPalindrome = function (s) {
 // Runtime: 79 ms, faster than 89.76 % of JavaScript online submissions for Valid Palindrome.
 // Memory Usage: 47.4 MB, less than 30.93 % of JavaScript online submissions for Valid Palindrome.
 
+
+//two pointers
+
+var isPalindrome = function (s) {
+  let l = 0;
+  let r = s.length - 1;
+  while (l < r) {
+    while (l < r && isNonAlphaNumeric(s[l].toLowerCase())) l++;
+    while (l < r && isNonAlphaNumeric(s[r].toLowerCase())) r--;
+    if (s[l].toLocaleLowerCase() !== s[r].toLocaleLowerCase()) return false;
+    l++;
+    r--;
+  }
+  return true;
+};
+
+const isNonAlphaNumeric = (char) => {
+  const isNonAlpha = char < 'a' || 'z' < char; // a(97) - z(122)
+  const isNonNumeric = char < '0' || '9' < char; // 0(48) - 9(57)
+
+  return isNonAlpha && isNonNumeric;
+};
+
+// Runtime: 88 ms Beats: 83.75 %
+// Memory: 45.3 MB Beats: 59.27 %
+
 module.exports = { isPalindrome }
