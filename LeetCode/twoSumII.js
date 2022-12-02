952c5919dc7ba0e2853568b6417734bfcf471d1f
jf <jpf0628@gmail.com>
2022-12-01 16:02:48 -0800

    added solution twosum

--- LeetCode/twoSumII.js
+++ LeetCode/twoSumII.js
@@ -8,7 +8,7 @@
 var twoSumII = function (numbers, target) {
   let map = {};
   for (let i = 0; i < numbers.length; i++) {
-    let diff = target - numbers[i];
+    let diff = target - numbers[i];x``
     if (diff in map) {
       return [map[diff] + 1, i + 1];
     } else {
@@ -24,3 +24,18 @@ module.exports = { twoSumII };
 // Two Sum II - Input Array Is Sorted.
 // Memory Usage: 43.3 MB, less than 12.95 % of JavaScript online submissions
 //  for Two Sum II - Input Array Is Sorted.
+
+// two pointer ===> exceeds time limit
+
+  // var twoSum = function (numbers, target) {
+  //   let l = 0;
+  //   let r = 1;
+  //   while (l < r) {
+  //     if (numbers[l] + numbers[r] === target) return [l + 1, r + 1];
+  //     if (r === numbers.length - 1) {
+  //       l++;
+  //       r = l + 1;
+  //     }
+  //     r++;
+  //   }
+  // };
