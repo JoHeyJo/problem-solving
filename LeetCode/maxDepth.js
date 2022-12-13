9c51e521a15a7624028b2e51fdd69cf901a61554
jf <jpf0628@gmail.com>
2022-12-13 11:08:20 -0800

    #33: Maximum Depth of Binary Tree

---
+++ LeetCode/maxDepth.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  if (!root) return null;
+
+  return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
+
+// Runtime
+// 130 ms
+// Beats
+// 13.78 %
+
+//   Memory
+// 45.5 MB
+// Beats
+// 40.97 %
